3893450eb3ed24208839d2724cb06d1503d25a41
kratman <kratmandu@gmail.com>
2016-04-30 11:55:28 -0400

    Replacing sliders with text boxes so that it compiles

--- ZOModelApp/JavaApp/ZOModelApp.java
+++ ZOModelApp/JavaApp/ZOModelApp.java
@@ -25,21 +25,21 @@ public class ZOModelApp
     int framePosCt = 0; //Counter for the position in the frame
 
     //Initialize ZOMOdds variables
-    double oddsZomPer = 0.0; //Zombie population for ZOMOdds
-    double oddsFightPer = 0.0; //Fighting percentage for ZOMOdds
+    double oddsZomPer = 90.0; //Zombie population for ZOMOdds
+    double oddsFightPer = 50.0; //Fighting percentage for ZOMOdds
     double oddsNumZom = 0.0; //Output for ZOMOdds
     double oddsWinPer = 0.0; //Output for ZOMOdds
 
     //Initialize ZOMlog variables
-    double logHPop = 0.0; //Human population for ZOMlog
-    double logZPop = 0.0; //Zombie population for ZOMlog
-    double logYears = 0.0; //Simulation time in years for ZOMlog
-    double logMonths = 0.0; //Simulation time in months for ZOMlog
-    double logPopRate = 0.0; //Population growth rate for ZOMlog
-    double logWinRate = 0.0; //Human win rate for ZOMlog
-    double logInfRate = 0.0; //Natural infection rate for ZOMlog
-    double logMerRate = 0.0; //Mercy rate for ZOMlog
-    double logEroRate = 0.0; //Zombie erosion rate for ZOMlog
+    double logHPop = 7000000.0; //Human population for ZOMlog
+    double logZPop = 100.0; //Zombie population for ZOMlog
+    double logYears = 5.0; //Simulation time in years for ZOMlog
+    double logMonths = 60.0; //Simulation time in months for ZOMlog
+    double logPopRate = 0.02; //Population growth rate for ZOMlog
+    double logWinRate = 0.60; //Human win rate for ZOMlog
+    double logInfRate = 0.02; //Natural infection rate for ZOMlog
+    double logMerRate = 0.10; //Mercy rate for ZOMlog
+    double logEroRate = 0.05; //Zombie erosion rate for ZOMlog
     double logCompScl = 10000400.0; //Human-human cooperation for ZOMlog
     double logApocCyc = 1200000000.0; //Months between extinctions for ZOMlog
     boolean logZomApoc = true; //ZOMlog flag to check for zombies
@@ -80,21 +80,21 @@ public class ZOModelApp
     oddsFightPerSlide.setPaintTicks(true);
     oddsFightPerSlide.setPaintLabels(true);
     //Add items to the window
-    intFrame.add(oddsInputText,SwingConstants.CENTER,framePosCt);
+    intFrame.add(oddsInputText,SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
-    intFrame.add(new JLabel("\n"),SwingConstants.CENTER,framePosCt);
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
-    intFrame.add(oddsZomPerLabel,SwingConstants.CENTER,framePosCt);
+    intFrame.add(oddsZomPerLabel,SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
-    intFrame.add(oddsZomPerSlide,SwingConstants.CENTER,framePosCt);
+    intFrame.add(oddsZomPerSlide,SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
-    intFrame.add(new JLabel("\n"),SwingConstants.CENTER,framePosCt);
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
-    intFrame.add(oddsFightPerLabel,SwingConstants.CENTER,framePosCt);
+    intFrame.add(oddsFightPerLabel,SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
-    intFrame.add(oddsFightPerSlide,SwingConstants.CENTER,framePosCt);
+    intFrame.add(oddsFightPerSlide,SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
-    intFrame.add(new JLabel("\n\n\n"),SwingConstants.CENTER,framePosCt);
+    intFrame.add(new JLabel("\n\n\n"),SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
 
     //Prompt for ZOMlog input
@@ -104,63 +104,106 @@ public class ZOModelApp
     //Create text box for logHPop
     JLabel logHPopLabel = new JLabel("Human population (thousands):");
     logHPopLabel.setFont(logHPopLabel.getFont().deriveFont(12.0f));
-    
+    JTextField logHPopBox = new JTextField("7000000",10);
     //Create text box for logZPop
-    JLabel logHPopLabel = new JLabel("Human population (thousands):");
-    logHPopLabel.setFont(logHPopLabel.getFont().deriveFont(12.0f));
-    
+    JLabel logZPopLabel = new JLabel("Zombie population (thousands):");
+    logZPopLabel.setFont(logZPopLabel.getFont().deriveFont(12.0f));
+    JTextField logZPopBox = new JTextField("100",10);
     //Create text box for logYears
-    JLabel logHPopLabel = new JLabel("Length of simulation (years):");
-    logHPopLabel.setFont(logHPopLabel.getFont().deriveFont(12.0f));
-    
+    JLabel logYearsLabel = new JLabel("Length of simulation (years):");
+    logYearsLabel.setFont(logYearsLabel.getFont().deriveFont(12.0f));
+    JTextField logYearsBox = new JTextField("5",10);
     //Create slider for logPopRate
     JLabel logPopRateLabel = new JLabel("Fractional population growth rate:");
     logPopRateLabel.setFont(logPopRateLabel.getFont().deriveFont(12.0f));
-    JSlider logPopRateSlide = new JSlider(JSlider.HORIZONTAL,0,0.2,0.02);
-    logPopRateSlide.setMajorTickSpacing(0.05);
-    logPopRateSlide.setMinorTickSpacing(0.01);
-    logPopRateSlide.setPaintTicks(true);
-    logPopRateSlide.setPaintLabels(true);
+    JTextField logPopRateBox = new JTextField("0.02",10);
     //Create slider for logWinRate
-    JLabel logWinRateLabel = new JLabel("Probability of humans killing a "
+    JLabel logWinRateLabel = new JLabel("Probability of a human killing a "
                                         +"zombie:");
     logWinRateLabel.setFont(logWinRateLabel.getFont().deriveFont(12.0f));
-    JSlider logWinRateSlide = new JSlider(JSlider.HORIZONTAL,0,1,0.5);
-    logWinRateSlide.setMajorTickSpacing(0.1);
-    logWinRateSlide.setMinorTickSpacing(0.05);
-    logWinRateSlide.setPaintTicks(true);
-    logWinRateSlide.setPaintLabels(true);
+    JTextField logWinRateBox = new JTextField("0.60",10);
     //Create slider for logInfRate
     JLabel logInfRateLabel = new JLabel("Yearly natural infection rate:");
     logInfRateLabel.setFont(logInfRateLabel.getFont().deriveFont(12.0f));
-    JSlider logInfRateSlide = new JSlider(JSlider.HORIZONTAL,0,0.2,0.02);
-    logInfRateSlide.setMajorTickSpacing(0.05);
-    logInfRateSlide.setMinorTickSpacing(0.01);
-    logInfRateSlide.setPaintTicks(true);
-    logInfRateSlide.setPaintLabels(true);
+    JTextField logInfRateBox = new JTextField("0.02",10);
     //Create slider for logMerRate
     JLabel logMerRateLabel = new JLabel("Proability that the infected are "
                                         +"prevented from reanimating:");
     logMerRateLabel.setFont(logMerRateLabel.getFont().deriveFont(12.0f));
-    JSlider logMerRateSlide = new JSlider(JSlider.HORIZONTAL,0,1,0.1);
-    logMerRateSlide.setMajorTickSpacing(0.25);
-    logMerRateSlide.setMinorTickSpacing(0.1);
-    logMerRateSlide.setPaintTicks(true);
-    logMerRateSlide.setPaintLabels(true);
+    JTextField logMerRateBox = new JTextField("0.10",10);
     //Create slider for logEroRate
     JLabel logEroRateLabel = new JLabel("Yearly natural zombie erosion rate:");
     logEroRateLabel.setFont(logEroRateLabel.getFont().deriveFont(12.0f));
-    JSlider logEroRateSlide = new JSlider(JSlider.HORIZONTAL,0.50,0.05);
-    logEroRateSlide.setMajorTickSpacing(0.05);
-    logEroRateSlide.setMinorTickSpacing(0.01);
-    logEroRateSlide.setPaintTicks(true);
-    logEroRateSlide.setPaintLabels(true);
+    JTextField logEroRateBox = new JTextField("0.05",10);
     //Add items to the window
-    intFrame.add(logInputText,SwingConstants.CENTER,framePosCt);
+    intFrame.add(logInputText,SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
-    intFrame.add(new JLabel("\n\n\n"),SwingConstants.CENTER,framePosCt);
+    intFrame.add(new JLabel("\n\n\n"),SwingConstants.LEFT,framePosCt);
     framePosCt += 1;
-    
+    intFrame.add(logHPopLabel,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logHPopBox,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logZPopLabel,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logZPopBox,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logYearsLabel,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logYearsBox,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logPopRateLabel,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logPopRateBox,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logWinRateLabel,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logWinRateBox,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logInfRateLabel,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logInfRateBox,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logMerRateLabel,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logMerRateBox,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logEroRateLabel,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(logEroRateBox,SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
+    intFrame.add(new JLabel("\n\n\n"),SwingConstants.LEFT,framePosCt);
+    framePosCt += 1;
 
     //Prompt for ZOModel input
     JLabel modelInputText = new JLabel("Settings for ZOModel:",
