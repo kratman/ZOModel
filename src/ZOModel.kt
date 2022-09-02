2f85cea1c848945082d47f2f6e7ea89269cf992c
kratman <kratmandu@gmail.com>
2022-09-01 22:17:08 -0400

    Adding some error handling

--- src/ZOModel.kt
+++ src/ZOModel.kt
@@ -3,13 +3,13 @@ package src
 class ZOModel {
     private var humans: Int = 0 // Human population
     private var zombies: Int = 0 // Zombie population
-    private var bitten: Int = 0 // Number of bitten humans
     private var days: Int = 0 // Number of days since the outbreak
     private var winProb: Double = 0.0 // Probability that the human kills the zombie
     private var eatProb: Double = 0.0 // Probability that the zombie eats the human
     private var biteProb: Double = 0.0 // Probability that the human was bitten
     private var mercyProb: Double = 0.0 // Probability for mercy
     private var infectProb: Double = 0.0 // Initial infection fraction
+    private var quitSimulation: Boolean = false // Avoids simulating erroneous input
 
     private val printToScreen: Boolean = true
 
@@ -52,15 +52,20 @@ class ZOModel {
         humans = 0
     }
 
-    fun runCalc() {
-        printHeader()
-        errorChecker()
-        while (((humans > 0) && (zombies > 0)) && (days == 0))
-        {
+    private fun simulation() {
+        while (((humans > 0) && (zombies > 0)) && (days == 0)) {
             printProgress()
             dailyUpdate()
         }
         printProgress()
+    }
+
+    fun runCalc() {
+        printHeader()
+        errorChecker()
+        if (!quitSimulation) {
+            simulation()
+        }
     }
 
     fun printResults() {
