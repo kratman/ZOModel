5c1eacca9f946f8252fb87e9df0a913a85cb7499
kratman <kratmandu@gmail.com>
2022-09-08 22:22:57 -0400

    Changing if-else to when

--- Main.kt
+++ Main.kt
@@ -6,32 +6,37 @@ import src.ZOModel
 
 fun main() {
     println("Pick a model to run [ZOModel, ZOMLog, ZOMOdds, VOMSpread]:")
-    val choice = readln().lowercase()
-    if (choice == "zomodel") {
-        val sim = ZOModel()
-        sim.togglePrinting()
-        sim.promptForInput()
-        sim.runCalc()
-        sim.printResults()
-    } else if (choice == "zomlog") {
-        val sim = ZOMLog()
-        sim.togglePrinting()
-        sim.promptForInput()
-        sim.runCalc()
-        sim.printResults()
-    } else if (choice == "zomodds") {
-        val sim = ZOMOdds()
-        sim.togglePrinting()
-        sim.promptForInput()
-        sim.runCalc()
-        sim.printResults()
-    } else if (choice == "vomspread") {
-        val sim = VOMSpread()
-        sim.togglePrinting()
-        sim.promptForInput()
-        sim.runCalc()
-        sim.printResults()
-    } else {
-        println("That is not an option.")
+    when (readln().lowercase()) {
+        "zomodel" -> {
+            val sim = ZOModel()
+            sim.togglePrinting()
+            sim.promptForInput()
+            sim.runCalc()
+            sim.printResults()
+        }
+        "zomlog" -> {
+            val sim = ZOMLog()
+            sim.togglePrinting()
+            sim.promptForInput()
+            sim.runCalc()
+            sim.printResults()
+        }
+        "zomodds" -> {
+            val sim = ZOMOdds()
+            sim.togglePrinting()
+            sim.promptForInput()
+            sim.runCalc()
+            sim.printResults()
+        }
+        "vomspread" -> {
+            val sim = VOMSpread()
+            sim.togglePrinting()
+            sim.promptForInput()
+            sim.runCalc()
+            sim.printResults()
+        }
+        else -> {
+            println("That is not an option.")
+        }
     }
 }
